96e0ac49448d02bf979592976c0f696e57112934
leeching <leeching.fx@gmail.com>
2019-03-19 10:44:14 +0800

    update response body

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const app = http.createServer();
 app.on('request', (req, res) => {
-    res.end('hello\n');
+    res.end('hello again\n');
 });
 
 const PORT = process.env.NODE_PORT || 8080;
